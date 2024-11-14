c6cee9ec5b2d3e0ec8b0b201083b4eb22266279c
gatteoelmo <matteogallardo99@gmail.com>
2024-11-14 16:26:08 +0000

    connected to mongodb

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
 import espress from "express";
 import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
 
 dotenv.config();
 const app = espress();
+connectDB();
+
 app.disable("x-powered-by");
 
 app.get("/", (req, res) => {
     res.json({ message: "Hello World!" });
 });
 
+
+// setting up routes
+app.use('/users', (req, res) => {
+    res.json({ message: "you're on the users page" });
+});
+
+app.use('/posts', (req, res) => {
+    res.json({ message: "you're on the posts page" });
+});
+
+app.use('/interactions', (req, res) => {
+    res.json({ message: "you're on the interactions page" });
+});
+
+
+// setting up a server 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
